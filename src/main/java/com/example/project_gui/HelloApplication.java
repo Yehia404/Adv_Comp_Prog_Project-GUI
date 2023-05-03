4a7ec1b59b6a6680e30fda30b126b43b3ca03e91
ahmedaymanali03 <21p0002@eng.asu.edu.eg>
2023-05-04 01:45:08 +0300

    tried around in class SceneSwitcher but error classcastexception still here added object from library in main of hello application as the main we made before doesnt intialize it

--- src/main/java/com/example/project_gui/HelloApplication.java
+++ src/main/java/com/example/project_gui/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.example.project_gui;
 
+import classes.Library;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -18,6 +19,7 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        Library Library = new Library();
         launch();
     }
 }
