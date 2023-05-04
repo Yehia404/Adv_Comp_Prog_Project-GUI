3d0752ad149c3406c37f2832060776c92172402f
“Yehia404” <Yehiasakr4676@gmail.com>
2023-05-05 00:58:49 +0300

    added searchBook/searchUser

---
+++ src/main/java/com/example/project_gui/SearchController.java
@@ -0,0 +1,51 @@
+package com.example.project_gui;
+import classes.*;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SearchController {
+
+    @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
+    @FXML
+    private Parent root;
+    @FXML
+    private TextField idTextField;
+    @FXML
+    private Button searchBtn;
+    private int id;
+    public void Search(ActionEvent event) throws IOException {
+        id = Integer.parseInt(idTextField.getText());
+
+        int i= Librarian.searchMember(id);
+
+        FXMLLoader loader= new FXMLLoader (getClass().getResource("SearchUser2.fxml"));
+        root=loader.load();
+        SearchController2 searchController2 = loader.getController();
+        searchController2.displayData(Library.persons.get(i).getFIRSTNAME(),Library.persons.get(i).getLASTNAME(),Library.persons.get(i).getEmail());
+
+
+        stage = (Stage)searchBtn.getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+    }
+    public void switchToInitialLibrarian(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("InitialLibrarian.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/main/java/com/example/project_gui/SearchController2.java
@@ -0,0 +1,49 @@
+package com.example.project_gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SearchController2 {
+    @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
+    @FXML
+    private Parent root;
+
+    @FXML
+    Label firstLabel;
+    @FXML
+    Label lastLabel;
+    @FXML
+    Label emailLabel;
+
+
+    public void displayData(String firstName, String lastName, String Email){
+    firstLabel.setText("First Name: "+ firstName);
+    lastLabel.setText("Last Name: "+ lastName);
+    emailLabel.setText("Email: "+ Email);
+}
+    public void switchToSearchUser(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("SearchUser.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToInitialLibrarian(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("InitialLibrarian.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
